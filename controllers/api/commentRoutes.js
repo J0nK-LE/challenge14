2957a9c3f2e8daa503b8a09062ffdad3136cabac
Jon Kelly <jonfkelly@gmail.com>
2022-10-27 15:54:43 -0500

    fixed delete button on comment page, update button works on the frontend but isnt making it to the backend

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -35,4 +35,25 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.post('/:id', withAuth, async (req, res) => {
+  console.log(req.body,"Update comment",req.params.id)
+  try {
+    const commentData = await Comment.update(req.body,{
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      }
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,9 +1,10 @@
 const usersComments = document.getElementById('usersComments');
 
+
 const delButtonHandler = async (event) => {
+    event.preventDefault()
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
-  
       const response = await fetch(`/api/comment/${id}`, {
         method: 'DELETE',
       });
@@ -16,8 +17,30 @@ const delButtonHandler = async (event) => {
     }
   };
 
+  const updateButtonHandler = async (event) => {
+    event.preventDefault()
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+      const comment_data = document.getElementById(id).value
+      console.log(id,comment_data)
+      const response = await fetch(`/api/comment/${id}`, {
+        method: 'POST',
+        body: comment_data 
+        // JSON.stringify({comment:comment_data})
+      });
+  
+      if (response.ok) {
+        //document.location.replace('/profile');
+      } else {
+        alert('Failed to update comment');
+      }
+    }
+  };
+
 document
   .querySelector('.deleteBtn')
-  .addEventListener('click', function() { delButtonHandler()
-    console.log("CLICK")
-})
+  .addEventListener('click', delButtonHandler)
+
+  document
+  .querySelector('.updateBtn')
+  .addEventListener('click', updateButtonHandler)
